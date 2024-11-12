a85e9077d9d55631a3c0a4337ff7d1ccefbcaa15
Jake <jakevvw@qq.com>
2024-11-12 21:39:32 +0800

    tmp finish

--- core/src/common/engines/sled_backend.rs
+++ core/src/common/engines/sled_backend.rs
@@ -23,6 +23,7 @@ const META_KEY_PREFIX_ALLOCATOR: [u8; 1] = [u8::MIN];
 
 static HDR: LazyLock<DB> = LazyLock::new(|| sled_db_open().unwrap());
 
+#[derive(Debug)]
 pub struct SledEngine {
     hdr: &'static DB,
     prefix_allocator: PreAllocator,
@@ -75,7 +76,7 @@ impl Engine for SledEngine {
         let (prefix_allocator, initial_value) = PreAllocator::init();
 
         if hdr.get(META_KEY_MAX_KEYLEN).c(d!())?.is_none() {
-            hdr.insert(META_KEY_MAX_KEYLEN, 0_usize.to_string().as_bytes())
+            hdr.insert(META_KEY_MAX_KEYLEN, 0_usize.to_be_bytes().to_vec())
                 .c(d!())?;
         }
 
@@ -83,10 +84,15 @@ impl Engine for SledEngine {
             hdr.insert(prefix_allocator.key, initial_value.to_vec())
                 .unwrap();
         }
-        let max_keylen = AtomicUsize::new(crate::parse_int!(
-            hdr.get(META_KEY_MAX_KEYLEN).unwrap().unwrap().as_ref(),
-            usize
-        ));
+        let max_keylen = usize::from_be_bytes(
+            hdr.get(META_KEY_MAX_KEYLEN)
+                .unwrap()
+                .unwrap()
+                .as_ref()
+                .try_into()
+                .unwrap(),
+        );
+        let max_keylen = AtomicUsize::new(max_keylen);
 
         Ok(SledEngine {
             hdr,
@@ -133,34 +139,48 @@ impl Engine for SledEngine {
         if key.len() > self.get_max_keylen() {
             self.set_max_key_len(key.len());
         }
+        println!("insert key: {:?}, value: {:?}", key, value);
+        let mut k = hdr_prefix.to_vec();
+        k.extend_from_slice(key);
         let area_tree = self.get_area_tree(hdr_prefix);
-        let old_v = area_tree.get(key).unwrap();
-        area_tree.insert(key, value).unwrap();
-        let old_v = old_v.map(|iv| iv.as_ref().to_vec());
+        let old_v = area_tree.get(&k).unwrap().map(|iv| iv.as_ref().to_vec());
+        area_tree.insert(k, value).unwrap();
         old_v
     }
 
     fn get(&self, hdr_prefix: PreBytes, key: &[u8]) -> Option<RawValue> {
         let area_tree = self.get_area_tree(hdr_prefix);
-        let v = area_tree.get(key).unwrap();
-        v.map(|iv| iv.as_ref().to_vec())
+        let mut k = hdr_prefix.to_vec();
+        k.extend_from_slice(key);
+        area_tree.get(k).unwrap().map(|iv| iv.as_ref().to_vec())
     }
 
     fn remove(&self, hdr_prefix: PreBytes, key: &[u8]) -> Option<RawValue> {
+        let mut k = hdr_prefix.to_vec();
+        k.extend_from_slice(key);
         let area_tree = self.get_area_tree(hdr_prefix);
-        let old_v = area_tree.get(key).unwrap();
-        area_tree.remove(key).unwrap();
+        let old_v = area_tree.get(&k).unwrap();
+        println!("remve key: {:?}, value: {:?}", key, old_v);
+        area_tree.remove(k).unwrap();
         let old_v = old_v.map(|iv| iv.as_ref().to_vec());
         old_v
     }
 
     fn get_instance_len_hint(&self, instance_prefix: PreBytes) -> u64 {
-        let tree = self.get_area_tree(instance_prefix);
-        tree.len() as u64
+        let l =  self.get_area_tree(instance_prefix)
+            .get(instance_prefix)
+            .unwrap().unwrap();
+        let l = u64::from_be_bytes(l.as_ref().try_into().unwrap());
+        println!("get instance_prefix: {:?} len: {:?}", instance_prefix, l);
+        l
     }
 
-    #[allow(unused_variables)]
-    fn set_instance_len_hint(&self, instance_prefix: PreBytes, new_len: u64) {}
+    fn set_instance_len_hint(&self, instance_prefix: PreBytes, new_len: u64) {
+        println!("set instance_prefix: {:?} len:{}", instance_prefix,new_len);
+         self.get_area_tree(instance_prefix)
+            .insert(instance_prefix, new_len.to_be_bytes().to_vec())
+            .unwrap();
+    }
     fn iter(&self, hdr_prefix: PreBytes) -> SledIter {
         let inner = self.get_area_tree(hdr_prefix).iter();
         SledIter { inner }
@@ -171,7 +191,8 @@ impl Engine for SledEngine {
         hdr_prefix: PreBytes,
         bounds: R,
     ) -> SledIter {
-        let inner = self.get_area_tree(hdr_prefix).range(bounds);
+        let area_tree = self.get_area_tree(hdr_prefix);
+      let inner = area_tree.range(bounds);
         SledIter { inner }
     }
 }
@@ -200,6 +221,7 @@ impl DoubleEndedIterator for SledIter {
 }
 
 // key of the prefix allocator in the 'meta'
+#[derive(Debug)]
 struct PreAllocator {
     key: [u8; 1],
 }
@@ -213,7 +235,6 @@ impl PreAllocator {
             (RESERVED_ID_CNT + Pre::MIN).to_be_bytes(),
         )
     }
-
     // fn next(base: &[u8]) -> [u8; PREFIX_SIZE] {
     //     (crate::parse_prefix!(base) + 1).to_be_bytes()
     // }
@@ -231,9 +252,49 @@ fn sled_db_open() -> Result<DB> {
         // fastest
         .mode(Mode::HighThroughput);
 
-    #[cfg(feature = "compress")]
-    let cfg = cfg.use_compression(true).compression_factor(20);
-
-    let db = cfg.open().c(d!())?;
+    // #[cfg(feature = "compress")]
+    // let cfg = cfg.use_compression(true).compression_factor(20);
+    let db = cfg.open().c(d!(dir.to_str().unwrap()))?;
     Ok(db)
+}
+
+#[cfg(test)]
+mod unit_tests {
+    use core::str;
+    use std::fs;
+    fn clean_dir() {
+        let dir = vsdb_get_base_dir();
+        fs::remove_dir_all(dir.clone()).unwrap();
+    }
+
+    use super::*;
+    #[test]
+    fn sled_open_sleddb() {
+        clean_dir();
+        let db = sled_db_open();
+        assert!(db.is_ok(), "db open failed");
+        let db = db.unwrap();
+        //    let inser_ret =  db.insert(b"abc", b"value");
+        //    assert!( inser_ret.is_ok(),"insert failed");
+        let get_ret = db.get(b"abc");
+        assert!(get_ret.is_ok(), "get failed");
+        dbg!(String::from_utf8(get_ret.unwrap().unwrap().to_vec()));
+        //    assert_eq!(get_ret.unwrap().unwrap(), b"value","get value not match");
+    }
+    #[test]
+    fn sled_allocator_test() {
+        let (prefix_allocator, initial_value) = PreAllocator::init();
+        dbg!(&prefix_allocator);
+        dbg!(initial_value);
+        assert_eq!(
+            RESERVED_ID_CNT + Pre::MIN,
+            u64::from_be_bytes(initial_value)
+        );
+    }
+    #[test]
+    fn sled_new() {
+        let db = SledEngine::new();
+        assert!(db.is_ok());
+        let db = db.unwrap();
+    }
 }
